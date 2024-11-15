d010ef3761638eb11e087adcaf297b4dc7813a35
jmcoop18 <116752215+jmcoop18@users.noreply.github.com>
2024-11-15 14:48:51 -0500

    Update quest2.py

--- quest2.py
+++ quest2.py
@@ -10,29 +10,37 @@ for i in range(0, len(runic)):
 
 
 
-f = (open('test.txt').read().split('\n'))
+# Part 2
+f = (open('notes2-2.txt').read().split('\n'))
 runic = f[0].replace('WORDS:', '').split(',')
 text = f[2:]
 t = 0
+
+def occurences(string, substring):
+    t = 0
+    pos = 0
+    while pos <= len(string) - len(substring):
+        if string[pos:pos+len(substring)] == substring:
+            t += 1
+        pos += 1
+    return t
 
 for word in runic:
     if word[::-1] not in runic:
         runic.append(word[::-1])
 
 for line in text:
-    found = ''
-    for i in range(0, len(runic)):
-        if runic[i] in line:
-            found += (runic[i])
-        # print(runic[i][::-1])
+    found = []
+    for word in runic:
+        runicLength = len(word)
+        prev = 0
 
-    # print(found)
-    break
-
-
-    # t += len(set(found))
+        for i in range(occurences(line, word)):
+            idx = line.index(word, prev)
+            found.extend(range(idx, idx + runicLength))
+            prev = found[1-runicLength]    
     
-# print(t)
-
-
+    t += (len(set(found)))
+    
+print(t)
 
