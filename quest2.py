0065a680427ce52663a5cc88a63f97dd74440775
jmcoop18 <116752215+jmcoop18@users.noreply.github.com>
2024-11-18 14:57:00 -0500

    Update quest2.py

--- quest2.py
+++ quest2.py
@@ -1,4 +1,4 @@
-f = open('notes2-1.txt').read().split('\n\n')
+f = (open('notes2-1.txt').read().split('\n\n'))
 runic = f[0].replace('WORDS:', '').split(',')
 text = f[1]
 t = 0
@@ -29,7 +29,7 @@ def addReversedStrings(list):
             list.append(word[::-1])
     return list
 
-f = open('notes2-2.txt').read().split('\n')
+f = (open('notes2-2.txt').read().split('\n'))
 text = f[2:]
 runic = f[0].replace('WORDS:', '').split(',')
 runic = addReversedStrings(runic)
@@ -37,4 +37,41 @@ runic = addReversedStrings(runic)
 t = 0
 for phrase in text:
     t += occurences(phrase, runic)
-print(t)
+# print(t)
+
+
+
+# Part 3
+f = open('notes2-3.txt').read().split('\n')
+f = open('test.txt').read().split('\n')
+text = f[2:]
+runic = f[0].replace('WORDS:', '').split(',')
+runic = addReversedStrings(runic)
+
+def vertical(c):
+    s = ''
+    for ln in text:
+        s += ln[c]
+    return s
+
+def removeDuplicates(OG):
+    k = []
+    for c in OG:
+        if c not in k:
+            k.append(c)
+    return k
+
+def occurences_2D(string, substrings):
+    found = []
+    for substring in substrings:
+        pos = 0
+        while pos <= len(string) - len(substring):
+            if string[pos:pos+len(substring)] == substring:
+                found.extend(range(pos, pos+len(substring)))
+            pos += 1
+
+    return found
+
+coords = []
+for i in range(len(text)):
+    print(i, occurences_2D(text[i], runic))
