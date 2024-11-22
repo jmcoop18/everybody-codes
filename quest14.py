c9f87d410a40f48a1562202f5e77a1d653df0d7c
jmcoop18 <116752215+jmcoop18@users.noreply.github.com>
2024-11-22 14:21:19 -0500

    Update quest14.py

--- quest14.py
+++ quest14.py
@@ -1,35 +1,53 @@
-f = open('notes14-1.txt').read().split(',')
+f1 = [(i[0], int(i[1:])) for i in open('notes14-1.txt').read().split(',')]
+f2 = [[(i[0], int(i[1:])) for i in line.split(',')] for line in open('notes14-2.txt').read().split('\n')]
+f3 = [[(i[0], int(i[1:])) for i in line.split(',')] for line in open('notes14-3.txt').read().split('\n')]
 
-mx = 0
-ht = 0
-for d in f:
-    if d[0] == 'U':
-        ht += int(d[1:])
-    elif d[0] == 'D':
-        ht -= int(d[1:])
-    if ht > mx:
-        mx = ht  
-# print(mx)
+def grow(instructions):
+    x,y,z = 0,0,0
+    cds = set()
+    for d, n in instructions:
+        for _ in range(n):
+            match d:
+                case "U": y += 1
+                case "D": y -= 1
+                case "R": x += 1
+                case "L": x -= 1
+                case "F": z += 1
+                case "B": z -= 1
+            cds.add((x, y, z))
+    return cds, (x,y,z)
+
+print(max(y for x,y,z in grow(f1)[0]))
+print(len(set().union(* (grow(insts)[0] for insts in f2))))
+
+def findDist(start, targets, coords):
+    seen = set([start])
+    targets = set(targets)
+    t = 0
+    dist = 0
+    border = set([start])
+
+    while targets:
+        newborder = set()
+        for (x,y,z) in border:
+            seen.add((x,y,z))
+            if (x,y,z) in targets:
+                t += dist
+                targets.remove((x,y,z))
+            for (dx,dy,dz) in [(1,0,0),(-1,0,0),(0,1,0),(0,-1,0),(0,0,1),(0,0,-1)]:
+                nx,ny,nz = x+dx, y+dy, z+dz
+                if (nx,ny,nz) in coords and (nx,ny,nz) not in seen:
+                    newborder.add((nx,ny,nz))
+        dist += 1
+        border = newborder
+    return t
 
 
+blocks, leaves = set(), set()
+for insts in f3:
+    nblocks, nleaf = grow(insts)
+    blocks |= nblocks
+    leaves.add(nleaf)
+trunk = [(x,y,z) for (x,y,z) in blocks if x == z == 0]
+print(min(findDist(start, leaves, blocks) for start in trunk))
 
-f = [l.split(',') for l in open('notes14-2.txt').read().split('\n')]
-
-cds = {(0,0,0)}
-for ln in f:
-    for d in ln:
-        x, y, z = 0, 0, 0
-        if d[0] == 'U':
-            y += int(d[1:])
-        elif d[0] == 'D':
-            y -= int(d[1:])
-        elif d[0] == 'R':
-            x += int(d[1:])
-        elif d[0] == 'L':
-            x -= int(d[1:])
-        elif d[0] == 'F':
-            z += int(d[1:])
-        elif d[0] == 'B':
-            z -= int(d[1:])
-        cds.add((x, y, z))
-print(len(cds))
